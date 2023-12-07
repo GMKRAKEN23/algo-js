5a4fa56b500976b67ff57054448bac1dcc30f965
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2023-12-07 16:56:09 +0100

    exercice 2.2

---
+++ 2.2.js
@@ -0,0 +1,11 @@
+let number_one = prompt("Enter the first number");
+let number_two = prompt("Enter the second number");
+let number_three = prompt("Enter the third number");
+
+if(number_one >= number_two && number_two <= number_three){
+    console.log("the number two is between the number one and the number two");
+} else if(number_one > number_one){
+    console.log("answer invalide");
+}else{
+    console.log("the number two isn't between the number one and the number three");
+}
