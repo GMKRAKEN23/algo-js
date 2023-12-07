5f0461c2c453c4fdc49759fbf2acdfd606a6c808
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2023-12-07 15:08:10 +0100

    exercice 1.0

---
+++ 1.0.js
@@ -0,0 +1,8 @@
+let numberOne = 10;
+let numberTwo = 2;
+
+console.log(numberOne + numberTwo);
+console.log(numberOne - numberTwo);
+console.log(numberOne / numberTwo);
+console.log(numberOne * numberTwo);
+console.log(numberOne % numberTwo);
