5340cbf5ed276830d73280d41719dc5dde9903e7
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2023-12-08 12:14:27 +0100

    2.4.js

---
+++ let number = 1;.js
@@ -0,0 +1,7 @@
+for (let num = 1 ; num <= 100; num++){
+    if (num % 2 === 0){
+        console.log(num + "Nombre pair" + num /2);
+    }else{
+        console.log(num + "Nombre impair" + num * 3)
+    }
+}
