0b36044d2ee34eb54c1ec1231e9885fec9c2b26c
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2023-12-09 10:53:56 +0100

    3.1.js finished

---
+++ 3.1.js
@@ -0,0 +1,25 @@
+// Exercice 1
+
+let tab = [1, 2, 3, 4, 5];
+
+let sum = 0;
+
+for(let i = 0; i < tab.length; i++) {
+    sum +=tab[i];
+}
+
+console.log(sum);
+
+//Exercice 2
+
+let secondTab = [100, 101, 102];
+
+let secondSum = 0;
+
+for(let i = 0; i < secondTab.length; i++2){
+    secondSum += secondTab[i];
+}
+
+console.log(secondSum);
+
+g
